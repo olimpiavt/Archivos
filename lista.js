6f7d4576dffa74b96788509c89b17b87f32ec787
olimpiavt <123610522+olimpiavt@users.noreply.github.com>
2023-07-06 21:09:56 -0500

    Add files via upload

---
+++ lista.js
@@ -0,0 +1,51 @@
+[{
+"alumno": "Mariuxi", 
+"nota":2.50,
+"ranking":220
+},
+{
+"alumno": "Pedro", 
+"nota":9.09,
+"ranking":10
+},
+{
+"alumno":"Juan",
+"nota":4,
+"ranking":150
+},
+{
+"alumno": "Marcos",
+"nota":9.5, 
+"ranking":23
+},
+{
+"alumno": "Vanessa",
+"nota":7.75,
+"ranking":145
+},
+{
+"alumno": "Rossana",
+"nota":9.00,
+"ranking":155
+},
+{
+"alumno": "Elvis",
+"nota":5.75,
+"ranking":200
+},
+{
+"alumno": "Carmen",
+"nota":7.75,
+"ranking":145
+},
+{
+"alumno": "Mónica",
+"nota":6.54,
+"ranking":145
+},
+{
+"alumno": "Edgar",
+"nota":8.57,
+"ranking":310
+}
+]
